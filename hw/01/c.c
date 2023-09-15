0e720489ccf374a8d0dcec4a81efeb4d915b6c9b
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2023-09-15 12:35:51 +0800

    try to fix 01-c

--- hw/01/c.c
+++ hw/01/c.c
@@ -1,67 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main()
 {
-    int n, i, j, k;
-    char in[3][3][4];
-    int d[12], temp[12], w[12];
+    int n, i, j, k, max, fake;
+    char in[3][4];
+    int d[12], w[12];
     scanf("%d", &n);
 
     for (i = 0; i < n; i++) {
         memset(d, 0, sizeof(d));
         memset(w, 0, sizeof(w));
         for (j = 0; j < 3; j++) {
-            memset(temp, 0, sizeof(temp));
-            scanf("%s %s %s", in[j][0], in[j][1], in[j][2]);
-            switch(in[j][2][0])
-            {
-            case 'e': {
-                d[in[j][0][0] - 'A'] = 1;
-                d[in[j][0][1] - 'A'] = 1;
-                d[in[j][0][2] - 'A'] = 1;
-                d[in[j][0][3] - 'A'] = 1;
-                d[in[j][1][0] - 'A'] = 1;
-                d[in[j][1][1] - 'A'] = 1;
-                d[in[j][1][2] - 'A'] = 1;
-                d[in[j][1][3] - 'A'] = 1;
-                break;
-            }
-            case 'u': {
-                w[in[j][0][0] - 'A'] = temp[in[j][0][0] - 'A'] = 1;
-                w[in[j][0][1] - 'A'] = temp[in[j][0][1] - 'A'] = 1;
-                w[in[j][0][2] - 'A'] = temp[in[j][0][2] - 'A'] = 1;
-                w[in[j][0][3] - 'A'] = temp[in[j][0][3] - 'A'] = 1;
-                w[in[j][1][0] - 'A'] = temp[in[j][1][0] - 'A'] = -1;
-                w[in[j][1][1] - 'A'] = temp[in[j][1][1] - 'A'] = -1;
-                w[in[j][1][2] - 'A'] = temp[in[j][1][2] - 'A'] = -1;
-                w[in[j][1][3] - 'A'] = temp[in[j][1][3] - 'A'] = -1;
-                for (k = 0; k < 12; k++) {
-                    if (temp[k] == 0)
-                        d[k] = 1;
+            scanf("%s %s %s", in[0], in[1], in[2]);
+            for (k = 0; k < 4; k++) {
+                switch(in[2][0])
+                {
+                case 'e': {
+                    d[in[0][k] - 'A'] = 1;
+                    d[in[1][k] - 'A'] = 1;
+                    break;
                 }
-                break;
-            }
-            default: {
-                w[in[j][0][0] - 'A'] = temp[in[j][0][0] - 'A'] = -1;
-                w[in[j][0][1] - 'A'] = temp[in[j][0][1] - 'A'] = -1;
-                w[in[j][0][2] - 'A'] = temp[in[j][0][2] - 'A'] = -1;
-                w[in[j][0][3] - 'A'] = temp[in[j][0][3] - 'A'] = -1;
-                w[in[j][1][0] - 'A'] = temp[in[j][1][0] - 'A'] = 1;
-                w[in[j][1][1] - 'A'] = temp[in[j][1][1] - 'A'] = 1;
-                w[in[j][1][2] - 'A'] = temp[in[j][1][2] - 'A'] = 1;
-                w[in[j][1][3] - 'A'] = temp[in[j][1][3] - 'A'] = 1;
-                for (k = 0; k < 12; k++) {
-                    if (temp[k] == 0)
-                        d[k] = 1;
+                case 'u': {
+                    if (d[in[0][k] - 'A'] == 0)
+                        w[in[0][k] - 'A']++;
+                    if (d[in[1][k] - 'A'] == 0)
+                        w[in[1][k] - 'A']--;
+                    break;
                 }
-            }
+                case 'd': {
+                    if (d[in[0][k] - 'A'] == 0)
+                        w[in[0][k] - 'A']--;
+                    if (d[in[1][k] - 'A'] == 0)
+                        w[in[1][k] - 'A']++;
+                    break;
+                }
+                }
             }
         }
+        max = -1;
+        fake = 0;
+        
         for (j = 0; j < 12; j++) {
-            if (d[j] == 0)
-                printf("%c is the counterfeit coin and it is %s. ", j + 'A', w[j] == -1 ? "light" : "heavy");
+            // printf("%d", w[j]);
+            if (d[j] != 0)
+                continue;
+            if (abs(w[j]) > max) {
+                max = abs(w[j]);
+                fake = j;
+            }
         }
+        printf("%c is the counterfeit coin and it is %s.\n", fake + 'A', w[fake] <= 0 ? "light" : "heavy");
     }
     return 0;
 }
