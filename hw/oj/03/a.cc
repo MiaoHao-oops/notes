4da93fd39c05fa516b6486ca6aa5c204d428d417
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2023-11-06 15:48:57 +0800

    update: a add number of songs

--- hw/oj/03/a.cc
+++ hw/oj/03/a.cc
@@ -4,6 +4,7 @@
 
 long long dp[60];
 long long a[60];
+int num[60];
 
 int main()
 {
@@ -12,25 +13,41 @@ int main()
 
     for (int x = 0; x < T; x++) {
         memset(dp, 0, sizeof(dp));
+        memset(num, 0, sizeof(num));
         scanf("%d%d", &n, &t);
 
         for (int i = 0; i < n; i++)
             scanf("%lld", &a[i]);
 
         dp[0] = a[0] < t ? a[0] : 0;
+        num[0] = a[0] < t ? 1 : 0;
         for (int i = 1; i < n; i++) {
             long long max1 = -1, max2 = -1;
+            int idx1 = 0, idx2 = 0;
             for (int j = 0; j < i; j++) {
-                if (max1 < dp[j])
+                if (max1 < dp[j]) {
                     max1 = dp[j];
+                    idx1 = j;
+                }
             }
             for (int j = 0; j < i; j++) {
-                if (max2 < dp[j] + a[i] && dp[j] + a[i] < t)
+                if (max2 < dp[j] + a[i] && dp[j] + a[i] < t) {
                     max2 = dp[j] + a[i];
+                    idx2 = j;
+                }
             }
-            max1 = std::max(max1, a[i]);
-            dp[i] = std::max(max1, max2);
+            if (a[i] > max2 && a[i] < t) {
+                max2 = a[i];
+                idx2 = i;
+            }
+            if (max1 > max2) {
+                dp[i] = max1;
+                num[i] = num[idx1];
+            } else {
+                dp[i] = max2;
+                num[i] = num[idx2] + 1;
+            }
         }
-        printf("%lld\n", dp[n - 1] + 678);
+        printf("%d %lld\n", num[n - 1] + 1, dp[n - 1] + 678);
     }
 }
