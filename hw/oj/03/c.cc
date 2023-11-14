532d0091a75c459d62ad0cd9aaa5100711842621
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2023-11-14 08:53:07 +0800

    update: 03-c AC

--- hw/oj/03/c.cc
+++ hw/oj/03/c.cc
@@ -1,11 +1,31 @@
 #include <algorithm>
 #include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
 
 // #define DEBUG
 
 long long grid[11][101];
 long long dp[11][101];
-int p[11][101];
+// int p[11][101];
+std::vector<int> p[11][101];
+
+#define min(a, b) ((cmp(a, b) < 0) ? (a) : (b))
+
+int cmp(std::vector<int> a, std::vector<int>b)
+{
+    int i = 0, j = 0;
+    while (i < a.size() && j < b.size()) {
+        if (a[i] == b[j]) {
+            i++, j++;
+        } else {
+            return a[i] - b[j];
+        }
+    }
+
+    return (i == a.size() && j == a.size()) ? 0 : a.size() - b.size();
+}
 
 int main()
 {
@@ -15,26 +35,32 @@ int main()
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 scanf("%lld", &grid[i][j]);
+                p[i][j].clear();
             }
             dp[i][0] = grid[i][0];
+            p[i][0].push_back(i + 1);
         }
 
         for (int j = 1; j < n; j++) {
             for (int i = 0; i < m; i++) {
-                int path[3], row[3];
+                long long path[3];
+                std::vector<int> row[3];
                 path[0] = i == 0 ? dp[m - 1][j - 1] : dp[i - 1][j - 1];
-                row[0] = i == 0 ? m - 1 : i - 1;
+                row[0] = i == 0 ? p[m - 1][j - 1] : p[i - 1][j - 1];
                 path[1] = dp[i][j - 1];
-                row[1] = i;
+                row[1] = p[i][j - 1];
                 path[2] = i == (m - 1) ? dp[0][j - 1] : dp[i + 1][j - 1];
-                row[2] = i == (m - 1) ? 0 : i + 1;
+                row[2] = i == (m - 1) ? p[0][j - 1] : p[i + 1][j - 1];
+                row[0].push_back(i + 1);
+                row[1].push_back(i + 1);
+                row[2].push_back(i + 1);
                 if (path[0] < path[1]) {
                     if (path[0] < path[2]) {
                         dp[i][j] = path[0] + grid[i][j];
                         p[i][j] = row[0];
                     } else if (path[0] == path[2]) {
                         dp[i][j] = path[0] + grid[i][j];
-                        p[i][j] = std::min(row[0], row[2]);
+                        p[i][j] = min(row[0], row[2]);
                     } else {
                         dp[i][j] = path[2] + grid[i][j];
                         p[i][j] = row[2];
@@ -45,7 +71,7 @@ int main()
                         p[i][j] = row[1];
                     } else if (path[1] == path[2]) {
                         dp[i][j] = path[1] + grid[i][j];
-                        p[i][j] = std::min(row[1], row[2]);
+                        p[i][j] = min(row[1], row[2]);
                     } else {
                         dp[i][j] = path[2] + grid[i][j];
                         p[i][j] = row[2];
@@ -53,56 +79,28 @@ int main()
                 } else {
                     if (path[2] == path[0]) {
                         dp[i][j] = path[2] + grid[i][j];
-                        p[i][j] = std::min(row[0], std::min(row[1], row[2]));
+                        p[i][j] = min(row[0], min(row[1], row[2]));
                     } else if (path[2] < path[0]) {
                         dp[i][j] = path[2] + grid[i][j];
                         p[i][j] = row[2];
                     } else {
                         dp[i][j] = path[1] + grid[i][j];
-                        p[i][j] = std::min(row[0], row[1]);
+                        p[i][j] = min(row[0], row[1]);
                     }
                 }
             }
         }
-#ifdef DEBUG
-        printf("\n");
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                printf("%2lld ", dp[i][j]);
-            }
-            printf("\n");
-        }
 
-        printf("\n");
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                printf("%2d ", p[i][j]);
-            }
-            printf("\n");
-        }
-#endif
-
-        long long min = 0xffffffff, pmin = 0xffffffff;
-        int idx = 0;
-        for (int i = 0; i < m; i++)
-            if (min > dp[i][n - 1] || pmin > p[i][n - 1] && min == dp[i][n - 1]) {
-                min = dp[i][n - 1], idx = i, pmin = p[i][n - 1];
+        long long min = dp[0][n - 1];
+        std::vector<int> pmin = p[0][n - 1];
+        for (int i = 1; i < m; i++)
+            if (min > dp[i][n - 1] || cmp(p[i][n - 1], pmin) < 0 && min == dp[i][n - 1]) {
+                min = dp[i][n - 1], pmin = p[i][n - 1];
             }
 
-        int path[1001];
-        path[n - 1] = idx;
-        for (int j = n - 2, i = idx; j >= 0; j--) {
-            path[j] = p[i][j + 1];
-            i = path[j];
-        }
-
-#ifdef DEBUG
-        printf("n = %d\n", n);
-#endif
-        for (int j = 0; j < n - 1; j++)
-            printf("%d ", path[j] + 1);
-
-        printf("%d", path[n - 1] + 1);
+        for (int i = 0; i < pmin.size() - 1; i++)
+            printf("%d ", pmin[i]);
+        printf("%d", pmin[pmin.size() - 1]);
 
         printf("\n%lld\n", min);
     }
